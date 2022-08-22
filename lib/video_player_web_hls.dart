4eade01f00b74e3afa0ed95a3080e3229dee426e
Harly <harly4444@getti.cn>
2022-08-22 16:05:02 +0800

    autoplay

--- lib/video_player_web_hls.dart
+++ lib/video_player_web_hls.dart
@@ -243,6 +243,7 @@ class _VideoPlayer {
     videoElement.setAttribute('id', 'videoPlayer');
     videoElement.setAttribute('x5-video-player-type', 'h5');
     videoElement.setAttribute('preload', 'auto');
+    videoElement.setAttribute('autoplay','true');
     videoElement.setAttribute('x5-video-player-fullscreen', 'true');
     videoElement.setAttribute('x-webkit-airplay', 'allow');
     videoElement.setAttribute('webkit-playsinline', 'true');
